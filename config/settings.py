ccc6339aa1a5f16cc0aef8106f0eaa86c0217940
Bart Römgens <bart.romgens@gmail.com>
2026-06-14 09:24:51 +0200

    Remove django-cors-headers, use Angular proxy instead

--- config/settings.py
+++ config/settings.py
@@ -16,13 +16,11 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",
-    "corsheaders",
     "api",
 ]
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
-    "corsheaders.middleware.CorsMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -83,8 +81,6 @@ REST_FRAMEWORK = {
         "rest_framework.permissions.AllowAny",
     ],
 }
-
-CORS_ALLOWED_ORIGINS: list[str] = []
 
 try:
     from .settings_local import *  # noqa: F401 F403
