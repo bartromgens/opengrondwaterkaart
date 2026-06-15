6179995805e5f2ca85038c3de69dabbcce4fbe15
Bart Römgens <bart.romgens@gmail.com>
2026-06-15 22:43:56 +0200

    Add back-to-map button on about page

--- client/src/app/about/about.ts
+++ client/src/app/about/about.ts
@@ -1,7 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-about',
+  imports: [RouterLink, MatButtonModule, MatIconModule],
   templateUrl: './about.html',
   styleUrl: './about.scss',
 })
